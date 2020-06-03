9086e468698d74335f2fb6493b08c21c75c3d599
Jakub Profota <profojak@fel.cvut.cz>
2020-06-03 10:42:07 +0200

    Initial setup

---
+++ README.md
@@ -0,0 +1,6 @@
+ap8 - actually perfect 8bit music sequencer
+
+## How to run
+
+To run this project on one of 7 mzapo kits from the `postel.felk.cvut.cz` server, run one of `./run/x.sh` shell scripts, where `x` denotes the position of the kit from left to right as seen on YouTube stream.
+
---
+++ main.c
@@ -0,0 +1,25 @@
+/********************************************************************
+  ap8 - actually perfect 8bit music sequencer
+
+  This is semestral work for B0B35APO course: Computer Architecture
+  at FEE CTU.
+
+  Made by Jakub Profota and Jenda Kucera in 2020 summer semester.
+********************************************************************/
+
+#define _POSIX_C_SOURCE 200112L
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "mzapo/mzapo_parlcd.h"
+#include "mzapo/mzapo_phys.h"
+#include "mzapo/mzapo_regs.h"
+#include "fonts/font_types.h"
+
+int main(int argc, char *argv[])
+{
+}
